ef754775539de870ed219210fb77724e3715b780
SejongPark <devsejong@gmail.com>
2016-10-14 18:28:18 +0900

    no message

---
+++ week05/src/test/java/net/chandol/study/oop/store/StoreTest.java
@@ -0,0 +1,17 @@
+package net.chandol.study.oop.store;
+
+
+import org.junit.Test;
+
+public class StoreTest {
+    @Test
+    public void 주문생성_테스트(){
+        // 사용자를 추가합니다.
+
+        // 상품을 추가합니다.
+
+        // 사용자에게 주문을 추가합니다.
+
+        // 만들어진 주문에 상품을 추가합니다.
+    }
+}
