421f8f297afa74757218324029efb447230c33a1
SejongPark <devsejong@gmail.com>
2016-10-14 18:32:28 +0900

    구조는 좀 더 간단하게

--- week05/src/main/java/net/chandol/study/oop/store/Member.java
+++ week05/src/main/java/net/chandol/study/oop/store/Member.java
@@ -2,15 +2,8 @@ package net.chandol.study.oop.store;
 
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 @Getter
-@Entity
 public class Member {
-    @Id
-    @GeneratedValue
     private Long id;
     private String name;
     private String email;
