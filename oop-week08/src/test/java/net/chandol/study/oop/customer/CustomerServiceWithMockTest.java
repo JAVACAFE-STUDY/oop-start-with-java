da3c69662972a0b98a97bd2bfc957b8fcaf08999
SejongPark <devsejong@gmail.com>
2016-11-10 21:28:25 +0900

    테스트코드 작성

--- oop-week08/src/test/java/net/chandol/study/oop/customer/CustomerServiceWithMockTest.java
+++ oop-week08/src/test/java/net/chandol/study/oop/customer/CustomerServiceWithMockTest.java
@@ -1,17 +1,24 @@
 package net.chandol.study.oop.customer;
 
+import net.chandol.study.oop.infra.EmailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerServiceTest {
+public class CustomerServiceWithMockTest {
+
+    @MockBean
+    EmailService emailService;
 
     @Autowired
     CustomerService customerService;
@@ -20,6 +27,7 @@ public class CustomerServiceTest {
     public void 회원가입테스트() {
         // given
         CustomerRequest request = new CustomerRequest("sejong@chandol.net", "p@ssw0rd");
+        Mockito.when(emailService.sendEmail(anyString(), anyString(), anyString())).thenReturn("가짜결과반환");
 
         // when
         customerService.signUp(request);
