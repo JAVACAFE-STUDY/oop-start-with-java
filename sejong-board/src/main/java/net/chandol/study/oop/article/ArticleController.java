be19dc940bbf382994dfdcfa5dc41bd26e2b43c2
sjpark <devsejong@gmail.com>
2016-11-22 17:49:33 +0900

    polish

--- sejong-board/src/main/java/net/chandol/study/oop/article/ArticleController.java
+++ sejong-board/src/main/java/net/chandol/study/oop/article/ArticleController.java
@@ -1,8 +1,8 @@
 package net.chandol.study.oop.article;
 
 import net.chandol.study.oop.article.dto.ArticleCreateRequest;
-import net.chandol.study.oop.article.dto.ArticlePageResponse;
 import net.chandol.study.oop.article.dto.ArticleModifyRequest;
+import net.chandol.study.oop.article.dto.ArticlePageResponse;
 import net.chandol.study.oop.article.model.Article;
 import net.chandol.study.oop.article.service.ArticleService;
 import org.modelmapper.ModelMapper;
@@ -20,6 +20,7 @@ public class ArticleController {
     @Autowired
     ModelMapper modelMapper;
 
+    // 게시물 만들기
     @PostMapping("/articles")
     public ResponseEntity<Article> createArticle(
             @RequestBody ArticleCreateRequest request) {
@@ -28,8 +29,19 @@ public class ArticleController {
         return ResponseEntity.ok(article);
     }
 
+    // 게시물 수정하기
+    @PutMapping("/articles/{articleId}")
+    public ResponseEntity<Article> modifyArticle(
+            @PathVariable("articleId") Article article,
+            ArticleModifyRequest request) {
+
+        Article modifiedArticle = articleService.modifyArticle(article, request);
+        return ResponseEntity.ok(modifiedArticle);
+    }
+
+    // 게시물 페이징 조회
     @GetMapping("/articles")
-    public ResponseEntity<Page<ArticlePageResponse>> pageableArticle(
+    public ResponseEntity<Page<ArticlePageResponse>> pageableArticles(
             @RequestParam(defaultValue = "0") int page) {
 
         Pageable pageable = new PageRequest(page, 10);
@@ -39,20 +51,12 @@ public class ArticleController {
         return ResponseEntity.ok(articlePageResponses);
     }
 
+    // 게시물 하나 조회
     @GetMapping("/articles/{articleId}")
     public ResponseEntity<Article> getArticle(
             @PathVariable("articleId") Article article) {
 
         return ResponseEntity.ok(article);
-    }
-
-    @PutMapping("/articles/{articleId}")
-    public ResponseEntity<Article> modifyArticle(
-            @PathVariable("articleId") Article article,
-            ArticleModifyRequest request) {
-
-        Article modifiedArticle = articleService.modifyArticle(article, request);
-        return ResponseEntity.ok(modifiedArticle);
     }
 
 }
